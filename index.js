09764bb0265aa8da4a7e9ee7aec35a3ce14b1fd8
Michael Born <mborn319@gmail.com>
2019-01-10 21:57:50 -0500

    Yay! Base node-starter app is working! :)
    So helpfully copied from https://github.com/wesbos/Learn-Node

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const routes = require('./routes/index');
+
 // Load environment vars into process.env.
 require('dotenv').config({ path: 'variables.env' });
+
+// create our Express app
+const app = express();
+
+// use our own routes defined in routes/index.js
+app.use('/', routes);
+
+// READY?! Let's go!
+
+
+// Start our app!
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
